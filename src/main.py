391b711dd7763c8f1f7d0004d6bc9ef8f3f89fcb
pemikels1 <park.mikels@gmail.com>
2021-06-26 20:44:00 -0500

    Update normalization mean and standard deviation

--- src/main.py
+++ src/main.py
@@ -30,7 +30,7 @@ def main():
 
     transform = transforms.Compose([transforms.Resize((300, 400)),
                                     transforms.ToTensor(),
-                                    transforms.Normalize((0.25, 0.25, 0.25), (0.25, 0.25, 0.25))
+                                    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
                                     ]
                                    )
 
