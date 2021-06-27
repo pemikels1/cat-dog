0e9e0eae5587b383973a45cd855d548b58b2369e
pemikels1 <park.mikels@gmail.com>
2021-06-26 20:41:54 -0500

    Update args hyperparameters

--- src/args.py
+++ src/args.py
@@ -6,10 +6,10 @@ def park_parse_args():
 
     parser = argparse.ArgumentParser(description='Cat Dog Classifier')
 
-    parser.add_argument('--batch_size', default=32, help='Number of observations sent at a time')
+    parser.add_argument('--batch_size', default=16, help='Number of observations sent at a time')
     parser.add_argument('--epochs', default=15, help='Number of passes through dataset')
     parser.add_argument('--shuffle', default=True, help='Shuffle data after every epoch')
-    parser.add_argument('--lr', default=0.35, help='Initial learning rate')
+    parser.add_argument('--lr', default=0.5, help='Initial learning rate')
     parser.add_argument('--step_size', default=2, help='Interval for LR rate decay')
     parser.add_argument('--gamma', default=0.7, help='LR rate decay factor')
     parser.add_argument('--train_percentage', default=0.8, help='Percentage of data in training set')
